fc3fa168f5cfcd1046ace18fdb8668020f9fdd50
devlucianogomes <dev.lucianogomes@gmail.com>
2022-02-17 17:54:15 -0300

    Resumo Objeto

---
+++ JS Basico/objetos.js
@@ -0,0 +1,31 @@
+// Objetos com Javascript
+
+// Quando criamos um objeto, parramos pra ele propriedades e métodos, atributos são características, e geralmente métodos, são ações,no exemplo abaixo, vamos ter um objeto de um carro. Lembrando que para criar o objetos, definimos uma variavel que recebe chaves, e dentro, suas informações
+
+// Ou seja, temos o objeto carro, que tem marca, modelo, ano de fabricação e também a informação se existe em estoque ou não
+
+let carro = {
+  marca: "Toyota",
+  modelo: "Corolla Xei 2.0",
+  anoDeFabricacao: 2019,
+  possuiEstoque: true,
+
+  // Recebendo funções como propriedades de um objeto
+  ligar: function () {
+    console.log("Carro ligado !");
+  },
+
+  acelerar: function () {
+    console.log("Vrum vrum vrum");
+  },
+};
+
+// Para acessar um obj, usamos a notação ponto.
+console.log(carro.modelo);
+
+// acessando as funcoes dentro do objeto
+carro.ligar();
+carro.acelerar();
+
+// USO DO THIS
+// this faz referencia ao proprio elemento que está em contexto, mas se atente, ele existe DENTRO DO OBJETO
